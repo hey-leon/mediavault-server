b5028d14cf96ffb14fdf5a231828a61f37bc058b
Leon Pearce <leonp1991@gmail.com>
2016-04-20 20:31:19 +1000

    cleaned up code with jslinter

--- _source/routes/restful.js
+++ _source/routes/restful.js
@@ -1,81 +1,79 @@
 import del from 'del'
 import path from 'path'
 
-import stash  from '../middleware/stash'
+import stash from '../middleware/stash'
 
-
-/* creates restful endpoint, with all base operations (see: node-restful) */
-export default (router, model) => {
-
-  router.route(`/${model.modelName}`)
+/* creates restful endpoint, with all base operations */
+export default (router, Model) => {
+  router.route(`/${Model.modelName}`)
 
     /* create */
-    .post(stash,  (req, res, next) => {
-      let file = req.file;
+    .post(stash, (req, res, next) => {
+      let file = req.file
 
-      let doc = new model({
-        name: req.file.originalname,
+      let doc = new Model({
+        name: file.originalname,
         owner: req.auth.user.email,
-        mimetype: req.file.mimetype,
-        uri: req.file.path
-      });
-
-      if(req.file.meta)
-        doc.meta = req.file.meta;
-
-      if(req.file.thumburi)
-        doc.thumburi = req.file.thumburi;
-
-      doc.save( (error) => {
-        if (error)
-          res.status(500).send(error);
-        else
-          res.status(200).send(doc)
+        mimetype: file.mimetype,
+        uri: file.path
       })
-      
+
+      if (file.meta) { doc.meta = file.meta }
+      if (file.thumburi) { doc.thumburi = req.file.thumburi }
+
+      doc.save((error) => {
+        if (error) {
+          res.status(500).send(error)
+        } else {
+          res.status(200).send(doc)
+        }
+      })
     })
 
     /* read */
-    .get( (req, res, next) => {
-      model.find({ owner: req.auth.user.email }, (error, docs) => {
-        if (error)
-          res.status(500).send(error);
-        else
+    .get((req, res, next) => {
+      Model.find({ owner: req.auth.user.email }, (error, docs) => {
+        if (error) {
+          res.status(500).send(error)
+        } else {
           res.status(200).send(docs)
+        }
       })
-    });
+    })
 
-
-  router.route(`/${model.modelName}/:id`)
+  router.route(`/${Model.modelName}/:id`)
 
     /* read by id */
     .get((req, res, next) => {
-      model.findById(req.params.id, (error, doc) => {
-        if (error)
-          res.status(500).send(error);
-        else
+      Model.findById(req.params.id, (error, doc) => {
+        if (error) {
+          res.status(500).send(error)
+        } else {
           res.status(200).send(doc)
+        }
       })
     })
 
     /* update */
     .patch((req, res, next) => {
-      model.findById(req.params.id, (error, doc) => {
-        if (error)
-          res.status(500).send(error);
-        else {
-          let attributes = model.schema.paths;
-          for(let attribute in attributes){
-              if(attribute in req.body)
-                doc.attribute = req.body[attribute]
+      Model.findById(req.params.id, (error, doc) => {
+        if (error) {
+          res.status(500).send(error)
+        } else {
+          let attributes = Model.schema.paths
+          for (let attribute in attributes) {
+            if (attribute in req.body) {
+              doc.attribute = req.body[attribute]
+            }
           }
-          doc.owner = req.auth.user.email;
+          doc.owner = req.auth.user.email
 
-          doc.save( (error) => {
-            if (error)
-              res.status(500).send(error);
-            else
+          doc.save((error) => {
+            if (error) {
+              res.status(500).send(error)
+            } else {
               res.sendStatus(200)
+            }
           })
         }
       })
@@ -83,25 +81,26 @@ export default (router, model) => {
 
     /* delete */
     .delete((req, res, next) => {
-      new Promise ((resolve) => {
-        model.findById(req.params.id, (error, doc) => {
-          if (error)
-            res.status(500).send(error);
-          else {
-            del(path.join(__dirname, '/../', doc.uri));
-            if (doc.thumburi)
+      new Promise((resolve) => {
+        Model.findById(req.params.id, (error, doc) => {
+          if (error) {
+            res.status(500).send(error)
+          } else {
+            del(path.join(__dirname, '/../', doc.uri))
+            if (doc.thumburi) {
               del(path.join(__dirname, '/../', doc.thumburi))
+            }
           }
           resolve()
         })
       }).then(() => {
-        model.findByIdAndRemove(req.params.id, (error) => {
-          if (error)
-            res.status(500).send(error);
-          else
+        Model.findByIdAndRemove(req.params.id, (error) => {
+          if (error) {
+            res.status(500).send(error)
+          } else {
             res.sendStatus(200)
+          }
         })
       })
     })
-
 }
--- _source/server.js
+++ _source/server.js
@@ -1,46 +1,38 @@
-import fs              from 'fs'
-import https           from 'https'
-import express         from 'express'
-import bodyParser      from 'body-parser'
-import mongoose        from 'mongoose'
+import fs from 'fs'
+import https from 'https'
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
 
-import authentication  from './middleware/authentication'
-import cors            from './middleware/cors'
-import restful         from './routes/restful'
-import account         from './routes/account'
-import endpoints       from './endpoints'
+import authentication from './middleware/authentication'
+import cors from './middleware/cors'
+import restful from './routes/restful'
+import account from './routes/account'
+import endpoints from './endpoints'
 
-let server  = express(),
-    port    = 3000,
-    api     = express.Router(),
-    data    = express.Router();
-
+let [ server, port, api, data ] = [ express(), 3000, express.Router(), express.Router() ]
 
 /* parse request body */
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 /* support cors */
-server.use(cors);
+server.use(cors)
 
 /* all request must be authenticated */
-server.use(authentication);
-
+server.use(authentication)
 
 /* connect to mongodb and start server */
-mongoose.connect('mongodb://localhost:27017/api');
+mongoose.connect('mongodb://localhost:27017/api')
+
+/* on db connect */
 mongoose.connection.once('open', () => {
+  for (let endpoint of endpoints) {
+    restful(api, endpoint)
+  }
 
-
-  for (let endpoint of endpoints)
-    restful(api, endpoint);
-
-  server.use('/api', api);
-
-  server.use('/account', account(data));
-
-  server.listen(port);
-
+  server.use('/api', api)
+  server.use('/account', account(data))
+  server.listen(port)
   console.log(`listening to port: ${port}`)
-
-});
+})
