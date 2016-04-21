02af56abc90757e3abaabe47fcdd0c5659fed275
Leon Pearce <leonp1991@gmail.com>
2016-04-21 16:14:08 +1000

    changes to cors

--- _source/middleware/cors.js
+++ _source/middleware/cors.js
@@ -4,21 +4,13 @@
 export default (req, res, next) => {
   // valid request origins
   let whitelist = ['http://mediavault.xyz', 'http://www.mediavault.xyz']
-  // test request origin
-  let origin
-  if (whitelist.indexOf(req.get('origin').toLowerCase()) > -1) {
-    origin = req.get('origin')
-  } else {
-    origin = whitelist[0]
-  }
   // set headers
-  res.header('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'authorization')
-  res.header('Access-Control-Allow-Credentials', 'true')
-  res.header('Access-Control-Allow-Origin', origin)
-  // if pre-flight? return request now : next middleware
   if (req.method === 'OPTIONS') {
-    return res.status(200).send()
+    if (whitelist.indexOf(req.get('origin').toLowerCase()) > -1) {
+      res.header('Access-Control-Allow-Origin', req.get('origin'))
+    }
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS')
+    res.send(200)
   } else {
     next()
   }
